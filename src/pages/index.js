03ff775d2089e99414ad11b8a05ce35f27bdabea
canodelacuadra <canodelacuadra@gmail.com>
2024-03-22 09:10:31 +0100

    Paso 3 utilizar Plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
  return (
    <Layout pageTitle='mi página de inicio'>
     <p>Aranjuez mola y mañana no lloverá</p>
-
+    <StaticImage
+       alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+       src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+     />
+     <StaticImage src="../images/estanquechinescos.jpg" alt='estanque de chinescos'/>
    </Layout>
  )
 }
